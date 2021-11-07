7cd0ba3b8efe03875943f5986c2405a60e93e5df
Ihtml <2483690020@qq.com>
2021-11-07 11:58:10 +0800

    feat(staticlinklist): add test case

--- Code/01线性表/03静态链表.cpp
+++ Code/01线性表/03静态链表.cpp
@@ -99,3 +99,44 @@ Status ListDelete(StaticLinkList L, int i) {
     Free_SSL(L, j);
     return OK;
 }
+
+Status ListTraverse(StaticLinkList L){
+    int j = 0;
+    int i = L[MAXSIZE - 1].cur;
+    while (i) {
+        visit(L[i].data);
+        i = L[i].cur;
+        j++;
+    }
+    return j;
+    printf("\n");
+    return OK;
+}
+
+int main(){
+    StaticLinkList L;
+    Status i;
+    i = InitList(L);
+    printf("初始化L后：L.length=%d\n", ListLength(L));
+
+    i = ListInsert(L, 1, 'F');
+    i = ListInsert(L, 1, 'E');
+    i = ListInsert(L, 1, 'D');
+    i = ListInsert(L, 1, 'B');
+    i = ListInsert(L, 1, 'A');
+
+    printf("\n在L的表头依次插入FEDBA后：\nL.data=");
+    ListTraverse(L);
+
+    i = ListInsert(L, 3, 'C');
+    printf("\n在L的“B”与“D”之间插入“C”后：\nL.data=");
+    ListTraverse(L);
+
+    i = ListDelete(L, 1);
+    printf("\n在L的删除“A”后：\nL.data=");
+    ListTraverse(L);
+
+    printf("\n");
+
+    return 0;
+}
