4a2e592d7e4b337df99f6842cfc7aef3f26838a4
Ihtml <2483690020@qq.com>
2021-11-09 09:30:29 +0800

    feat(stack): push pop and traverse

--- Code/02栈和队列/01顺序栈.cpp
+++ Code/02栈和队列/01顺序栈.cpp
@@ -50,3 +50,42 @@ int StackLength(SqStack S) {
     return S.top + 1;
 }
 
+/* 若栈不空，则用e返回S的栈顶元素，并返回OK；否则返回ERROR */
+Status GetTop(SqStack S, SElemType* e) {
+    if (S.top == -1)
+        return ERROR;
+    else
+        *e = S.data[S.top];
+    return OK;
+}
+
+/* 插入元素e为新的栈顶元素 */
+Status Push(SqStack* S, SElemType e) {
+    if (S->top == MAXSIZE - 1) /* 栈满 */
+    {
+        return ERROR;
+    }
+    S->top++;            /* 栈顶指针增加一 */
+    S->data[S->top] = e; /* 将新插入元素赋值给栈顶空间 */
+    return OK;
+}
+
+/* 若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR */
+Status Pop(SqStack* S, SElemType* e) {
+    if (S->top == -1)
+        return ERROR;
+    *e = S->data[S->top]; /* 将要删除的栈顶元素赋值给e */
+    S->top--;             /* 栈顶指针减一 */
+    return OK;
+}
+
+/* 从栈底到栈顶依次对栈中每个元素显示 */
+Status StackTraverse(SqStack S) {
+    int i;
+    i = 0;
+    while (i <= S.top) {
+        visit(S.data[i++]);
+    }
+    printf("\n");
+    return OK;
+}
