9300483f0c8f498618f14c0032d8e116110d2880
Ihtml <2483690020@qq.com>
2021-11-10 15:10:43 +0800

    feat(stack): add test case

--- Code/02栈和队列/01顺序栈.cpp
+++ Code/02栈和队列/01顺序栈.cpp
@@ -7,7 +7,7 @@
 #define OK 1
 #define ERROR 0
 #define TRUE 1
-#define FALSE 1
+#define FALSE 0
 #define MAXSIZE 20 /* 存储空间初始分配量 */
 
 typedef int Status;
@@ -88,4 +88,24 @@ Status StackTraverse(SqStack S) {
     }
     printf("\n");
     return OK;
+}
+
+int main() {
+    int j;
+    SqStack s;
+    int e;
+    if (InitStack(&s) == OK)
+        for (j = 1; j <= 10; j++)
+            Push(&s, j);
+    printf("栈中元素依次为：");
+    StackTraverse(s);
+    Pop(&s, &e);
+    printf("弹出的栈顶元素 e=%d\n", e);
+    printf("栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+    GetTop(s, &e);
+    printf("栈顶元素 e=%d 栈的长度为%d\n", e, StackLength(s));
+    ClearStack(&s);
+    printf("清空栈后，栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+
+    return 0;
 }
