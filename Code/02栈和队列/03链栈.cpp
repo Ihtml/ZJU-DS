e15d9b341f80134494388a45e96ec8fbf5a7ebda
Ihtml <2483690020@qq.com>
2021-11-16 11:51:34 +0800

    feat(linkstack): add test case

--- Code/02栈和队列/03链栈.cpp
+++ Code/02栈和队列/03链栈.cpp
@@ -96,3 +96,33 @@ Status Pop(LinkStack* S, SElemType* e) {
     S->count--;
     return OK;
 }
+
+Status StackTraverse(LinkStack S) {
+    LinkStackPtr p;
+    p = S.top;
+    while (p) {
+        visit(p->data);
+        p = p->next;
+    }
+    printf("\n");
+    return OK;
+}
+
+int main() {
+    int j;
+    LinkStack s;
+    int e;
+    if (InitStack(&s) == OK)
+        for (j = 1; j <= 10; j++)
+            Push(&s, j);
+    printf("栈中元素依次为：");
+    StackTraverse(s);
+    Pop(&s, &e);
+    printf("弹出的栈顶元素 e=%d\n", e);
+    printf("栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+    GetTop(s, &e);
+    printf("栈顶元素 e=%d 栈的长度为%d\n", e, StackLength(s));
+    ClearStack(&s);
+    printf("清空栈后，栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+    return 0;
+}
