27bc54dc0fe0b139c17483ac2e46f4b8c4f3bbed
Ihtml <2483690020@qq.com>
2021-11-19 09:51:31 +0800

    feat(queue): getHead EnQueue DeQueue

--- Code/02栈和队列/05顺序队列.cpp
+++ Code/02栈和队列/05顺序队列.cpp
@@ -50,3 +50,43 @@ Status QueueEmpty(SqQueue Q) {
 int QueueLength(SqQueue Q) {
     return (Q.rear - Q.front + MAXSIZE) % MAXSIZE;
 }
+
+/* 若队列不空,则用e返回Q的队头元素,并返回OK,否则返回ERROR */
+Status GetHead(SqQueue Q, QElemType* e) {
+    if (Q.front == Q.rear) /* 队列空 */
+        return ERROR;
+    *e = Q.data[Q.front];
+    return OK;
+}
+
+/* 若队列未满，则插入元素e为Q新的队尾元素 */
+Status EnQueue(SqQueue *Q, QElemType e) {
+    if ((Q->rear + 1) % MAXSIZE == Q->front) /* 队列满的判断 */
+        return ERROR;
+    Q->data[Q->rear] = e;              /* 将元素e赋值给队尾 */
+    Q->rear = (Q->rear + 1) % MAXSIZE; /* rear指针向后移一位置， */
+                                       /* 若到最后则转到数组头部 */
+    return OK;
+}
+
+/* 若队列不空，则删除Q中队头元素，用e返回其值 */
+Status DeQueue(SqQueue* Q, QElemType* e) {
+    if (Q->front == Q->rear) /* 队列空的判断 */
+        return ERROR;
+    *e = Q->data[Q->front];              /* 将队头元素赋值给e */
+    Q->front = (Q->front + 1) % MAXSIZE; /* front指针向后移一位置， */
+                                         /* 若到最后则转到数组头部 */
+    return OK;
+}
+
+/* 从队头到队尾依次对队列Q中每个元素输出 */
+Status QueueTraverse(SqQueue Q) {
+    int i;
+    i = Q.front;
+    while ((i + Q.front) != Q.rear) {
+        visit(Q.data[i]);
+        i = (i + 1) % MAXSIZE;
+    }
+    printf("\n");
+    return OK;
+}
