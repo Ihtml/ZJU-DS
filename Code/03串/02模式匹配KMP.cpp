b0a0ee2e2e09e9cecdd2f43f22e3bbf899af444a
Ihtml <2483690020@qq.com>
2021-12-02 10:51:31 +0800

    feat(kmp): 暴力法

--- Code/03串/02模式匹配KMP.cpp
+++ Code/03串/02模式匹配KMP.cpp
@@ -54,3 +54,27 @@ void NextPrint(int next[], int length) {
 int StrLength(String S) {
     return S[0];
 }
+
+/* 朴素的模式匹配法 */
+int Index(String S, String T, int pos) {
+    int i =
+        pos; /* i用于主串S中当前位置下标值，若pos不为1，则从pos位置开始匹配 */
+    int j = 1; /* j用于子串T中当前位置下标值 */
+    while (i <= S[0] &&
+           j <= T[0]) /* 若i小于S的长度并且j小于T的长度时，循环继续 */
+    {
+        if (S[i] == T[j]) /* 两字母相等则继续 */
+        {
+            ++i;
+            ++j;
+        } else /* 指针后退重新开始匹配 */
+        {
+            i = i - j + 2; /* i退回到上次匹配首位的下一位 */
+            j = 1;         /* j退回到子串T的首位 */
+        }
+    }
+    if (j > T[0])
+        return i - T[0];
+    else
+        return 0;
+}
