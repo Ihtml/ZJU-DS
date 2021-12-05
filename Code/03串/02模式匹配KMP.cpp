3d4c6a5ad9aae3d984a17b061d722ce760bad5f7
Ihtml <2483690020@qq.com>
2021-12-05 12:50:23 +0800

    feat(kmp): optimiszation

--- Code/03串/02模式匹配KMP.cpp
+++ Code/03串/02模式匹配KMP.cpp
@@ -82,38 +82,42 @@ int Index(String S, String T, int pos) {
 // KMP思想：利用好模式串本身隐含的信息，确定某个模式串元素失配的时候，怎么处理
 // 和主串没有关系,利用next数组进行匹配，主串指针不回溯
 /* 通过计算返回子串T的next数组。 */
-void get_next(String T, int *next){
-    int i = 1, j= 0;
-    next[1]=0;
+void get_next(String T, int* next) {
+    int i = 1, j = 0;
+    next[1] = 0;
     while (i < T[0]) /* 此处T[0]表示串T的长度 */
     {
-        if (j == 0 || T[i] == T[j]) /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
+        if (j == 0 ||
+            T[i] == T[j]) /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
         {
-            ++i; ++j;
-            next[i]=j; // 若Pi=Pj, 则next[j+1]=next[j]+1
-        }
-        else
-            j=next[j]; // 若字符不相同，则j回溯，令j=next[j],循环继续
-    } 
+            ++i;
+            ++j;
+            next[i] = j;  // 若Pi=Pj, 则next[j+1]=next[j]+1
+        } else
+            j = next[j];  // 若字符不相同，则j回溯，令j=next[j],循环继续
+    }
 }
 
 /* 返回子串T在主串S中第pos个字符之后的位置。若不存在，则函数返回值为0。 */
 /*  T非空，1≤pos≤StrLength(S)。 */
 int Index_KMP(String S, String T, int pos) {
-    int i = pos; /* i用于主串S中当前位置下标值，若pos不为1，则从pos位置开始匹配 */
+    int i =
+        pos; /* i用于主串S中当前位置下标值，若pos不为1，则从pos位置开始匹配 */
     // int i = 0;
     int j = 1;         /* j用于子串T中当前位置下标值 */
     int next[255];     /* 定义一next数组 */
     get_next(T, next); /* 对串T作分析，得到next数组 */
-    while (i <= S[0] && j <= T[0]) /* 若i小于S的长度并且j小于T的长度时，循环继续 */
+    while (i <= S[0] &&
+           j <= T[0]) /* 若i小于S的长度并且j小于T的长度时，循环继续 */
     {
-        if (j == 0 || S[i] == T[j]) /* 两字母相等则继续，与朴素算法增加了j=0判断 */
+        if (j == 0 ||
+            S[i] == T[j]) /* 两字母相等则继续，与朴素算法增加了j=0判断 */
         {
             ++i;
             ++j;
         } else           /* 指针后退重新开始匹配 */
             j = next[j]; /* j退回合适的位置，i值不变 */
-            // next[1] = 0   next[2] = 1
+                         // next[1] = 0   next[2] = 1
     }
     if (j > T[0])
         return i - T[0];
@@ -129,16 +133,35 @@ void get_nextval(String T, int* nextval) {
     nextval[1] = 0;
     while (i < T[0]) /* 此处T[0]表示串T的长度 */
     {
-        if (j == 0 || T[i] == T[j]) /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
+        if (j == 0 ||
+            T[i] == T[j]) /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
         {
             ++i;
             ++j;
             if (T[i] != T[j])   /* 若当前字符与前缀字符不同 */
                 nextval[i] = j; /* 则当前的j为nextval在i位置的值 */
             else
-                nextval[i] =            nextval[j]; /* 如果与前缀字符相同，则将前缀字符的 */
+                nextval[i] =
+                    nextval[j]; /* 如果与前缀字符相同，则将前缀字符的 */
                                 /* nextval值赋值给nextval在i位置的值 */
         } else
             j = nextval[j]; /* 若字符不相同，则j值回溯 */
     }
+}
+
+int Index_KMP1(String S, String T, int pos) {
+    int i = pos; /* i用于主串S中当前位置下标值，若pos不为1，则从pos位置开始匹配 */
+    int j = 1;   /* j用于子串T中当前位置下标值 */
+    int next[256]; /* 定义一next数组 */
+    get_nextval(T, next); /* 对串T作分析，得到next数组 */
+    while (i <= S[0] && j <= T[0]) { /* 若i小于S的长度并且j小于T的长度时，循环继续 */
+        if (j == 0 || S[i] == T[j]) { /* 两字母相等则继续，与朴素算法增加了j=0判断 */
+            ++i;
+            ++j;
+        } else /* 指针后退重新开始匹配 */
+            j = next[j]; /* j退回合适的位置，i值不变 */
+    }
+    if (j > T[0])
+        return i - T[0];
+    return 0;
 }
