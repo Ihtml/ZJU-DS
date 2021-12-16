67e31d0a0adbc81e714d78d865396001b698a80c
Ihtml <2483690020@qq.com>
2021-12-16 10:52:44 +0800

    feat(bitreelink): preorder inorder postorder traverse

--- Code/04树/02二叉树链式结构实现.cpp
+++ Code/04树/02二叉树链式结构实现.cpp
@@ -128,3 +128,33 @@ TElemType Value(BiTree p) {
 void Assign(BiTree p, TElemType value) {
     p->data = value;
 }
+
+/* 初始条件: 二叉树T存在 */
+/* 操作结果: 前序递归遍历T */
+void PreOrderTraverse(BiTree T) {
+    if (T == NULL)
+        return;
+    printf("%c", T->data); /* 显示结点数据，可以更改为其它对结点操作 */
+    PreOrderTraverse(T->lchild); /* 再先序遍历左子树 */
+    PreOrderTraverse(T->rchild); /* 最后先序遍历右子树 */
+}
+
+/* 初始条件: 二叉树T存在 */
+/* 操作结果: 中序递归遍历T */
+void InOrderTraverse(BiTree T) {
+    if (T == NULL)
+        return;
+    InOrderTraverse(T->lchild); /* 中序遍历左子树 */
+    printf("%c", T->data); /* 显示结点数据，可以更改为其它对结点操作 */
+    InOrderTraverse(T->rchild); /* 最后中序遍历右子树 */
+}
+
+/* 初始条件: 二叉树T存在 */
+/* 操作结果: 后序递归遍历T */
+void PostOrderTraverse(BiTree T) {
+    if (T == NULL)
+        return;
+    PostOrderTraverse(T->lchild); /* 先后序遍历左子树  */
+    PostOrderTraverse(T->rchild); /* 再后序遍历右子树  */
+    printf("%c", T->data); /* 显示结点数据，可以更改为其它对结点操作 */
+}
