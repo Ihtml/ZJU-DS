74f5f0d93f222b5ec442679ed35afea2737f87b2
Ihtml <2483690020@qq.com>
2021-12-20 11:05:37 +0800

    feat(threadbinarytree): inOrderThreading

--- Code/04树/03线索二叉树.cpp
+++ Code/04树/03线索二叉树.cpp
@@ -53,3 +53,44 @@ Status CreateBiThrTree(BiThrTree* T) {
     }
     return OK;
 }
+
+BiThrTree pre; /* 全局变量,始终指向刚刚访问过的结点 */
+/* 中序遍历进行中序线索化 */
+void InThreading(BiThrTree p) {
+    if (p) {
+        InThreading(p->lchild); /* 递归左子树线索化 */
+        if (!p->lchild)         /* 没有左孩子 */
+        {
+            p->LTag = Thread; /* 前驱线索 */
+            p->lchild = pre;  /* 左孩子指针指向前驱 */
+        }
+        if (!pre->rchild) /* 前驱没有右孩子 */
+        {
+            pre->RTag = Thread; /* 后继线索 */
+            pre->rchild = p; /* 前驱右孩子指针指向后继(当前结点p) */
+        }
+        pre = p;                /* 保持pre指向p的前驱 */
+        InThreading(p->rchild); /* 递归右子树线索化 */
+    }
+}
+
+/* 中序遍历二叉树T,并将其中序线索化,Thrt指向头结点 */
+Status InOrderThreading(BiThrTree* Thrt, BiThrTree T) {
+    *Thrt = (BiThrTree)malloc(sizeof(BiThrNode));
+    if (!*Thrt)
+        exit(OVERFLOW);
+    (*Thrt)->LTag = Link; /* 建头结点 */
+    (*Thrt)->RTag = Thread;
+    (*Thrt)->rchild = (*Thrt); /* 右指针回指 */
+    if (!T)                    /* 若二叉树空,则左指针回指 */
+        (*Thrt)->lchild = *Thrt;
+    else {
+        (*Thrt)->lchild = T;
+        pre = (*Thrt);
+        InThreading(T); /* 中序遍历进行中序线索化 */
+        pre->rchild = *Thrt;
+        pre->RTag = Thread; /* 最后一个结点线索化 */
+        (*Thrt)->rchild = pre;
+    }
+    return OK;
+}
