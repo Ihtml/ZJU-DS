1171dfd78558f83420833b128a79362c106b5416
Ihtml <2483690020@qq.com>
2021-12-24 10:13:07 +0800

    feat(graph): create marix graph

--- Code/05图/01邻接矩阵创建图.cpp
+++ Code/05图/01邻接矩阵创建图.cpp
@@ -20,3 +20,21 @@ typedef struct {
     int numNodes, numEdges;       /* 图中当前的顶点数和边数  */
 } MGraph;
 
+/* 建立无向网图的邻接矩阵表示 */
+void CreateMGraph(MGraph* G) {
+    int i, j, k, w;
+    printf("输入顶点数和边数:\n");
+    scanf("%d,%d", &G->numNodes, &G->numEdges); /* 输入顶点数和边数 */
+    for (i = 0; i < G->numNodes; i++) /* 读入顶点信息,建立顶点表 */
+        scanf(&G->vexs[i]);
+    for (i = 0; i < G->numNodes; i++)
+        for (j = 0; j < G->numNodes; j++)
+            G->arc[i][j] = INFINITY; /* 邻接矩阵初始化 */
+    for (k = 0; k < G->numEdges; k++) /* 读入numEdges条边，建立邻接矩阵 */
+    {
+        printf("输入边(vi,vj)上的下标i，下标j和权w:\n");
+        scanf("%d,%d,%d", &i, &j, &w); /* 输入边(vi,vj)上的权w */
+        G->arc[i][j] = w;
+        G->arc[j][i] = w; /* 因为是无向图，矩阵对称 */
+    }
+}
