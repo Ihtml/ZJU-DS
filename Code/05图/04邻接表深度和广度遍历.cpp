9d1290740bc50b6108288191c5455d29f84aaf4a
Ihtml <2483690020@qq.com>
2022-01-04 10:27:12 +0800

    feat(graph): createALGraph

--- Code/05图/04邻接表深度和广度遍历.cpp
+++ Code/05图/04邻接表深度和广度遍历.cpp
@@ -91,3 +91,77 @@ Status DeQueue(Queue* Q, int* e) {
     return OK;
 }
 /* ****************************************************** */
+
+void CreateMGraph(MGraph* G) {
+    int i, j;
+    G->numEdges = 15;
+    G->numVertexes = 9;
+
+    /* 读入顶点信息,建立顶点表 */
+    G->vexs[0] = 'A';
+    G->vexs[1] = 'B';
+    G->vexs[2] = 'C';
+    G->vexs[3] = 'D';
+    G->vexs[4] = 'E';
+    G->vexs[5] = 'F';
+    G->vexs[6] = 'G';
+    G->vexs[7] = 'H';
+    G->vexs[8] = 'I';
+    for (i = 0; i < G->numVertexes; i++) /* 初始化图 */
+    {
+        for (j = 0; j < G->numVertexes; j++) {
+            G->arc[i][j] = 0;
+        }
+    }
+    G->arc[0][1] = 1;
+    G->arc[0][5] = 1;
+    G->arc[1][2] = 1;
+    G->arc[1][8] = 1;
+    G->arc[1][6] = 1;
+    G->arc[2][3] = 1;
+    G->arc[2][8] = 1;
+    G->arc[3][4] = 1;
+    G->arc[3][7] = 1;
+    G->arc[3][6] = 1;
+    G->arc[3][8] = 1;
+    G->arc[4][5] = 1;
+    G->arc[4][7] = 1;
+    G->arc[5][6] = 1;
+    G->arc[6][7] = 1;
+
+    for (i = 0; i < G->numVertexes; i++) {
+        for (j = i; j < G->numVertexes; j++) {
+            G->arc[j][i] = G->arc[i][j];
+        }
+    }
+}
+
+/* 利用邻接矩阵构建邻接表 */
+void CreateALGraph(MGraph G, GraphAdjList* GL) {
+    int i, j;
+    EdgeNode* e;
+
+    *GL = (GraphAdjList)malloc(sizeof(graphAdjList));
+
+    (*GL)->numVertexes = G.numVertexes;
+    (*GL)->numEdges = G.numEdges;
+    for (i = 0; i < G.numVertexes; i++) /* 读入顶点信息,建立顶点表 */
+    {
+        (*GL)->adjList[i].in = 0;
+        (*GL)->adjList[i].data = G.vexs[i];
+        (*GL)->adjList[i].firstedge = NULL; /* 将边表置为空表 */
+    }
+
+    for (i = 0; i < G.numVertexes; i++) /* 建立边表 */
+    {
+        for (j = 0; j < G.numVertexes; j++) {
+            if (G.arc[i][j] == 1) {
+                e = (EdgeNode*)malloc(sizeof(EdgeNode));
+                e->adjvex = j; /* 邻接序号为j */
+                e->next =(*GL)->adjList[i].firstedge; /* 将当前顶点上的指向的结点指针赋值给e */
+                (*GL)->adjList[i].firstedge = e; /* 将当前顶点的指针指向e */
+                (*GL)->adjList[j].in++;
+            }
+        }
+    }
+}
