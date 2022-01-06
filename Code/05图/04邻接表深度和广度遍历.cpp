52ea8db0adb9aa81ab325cf0d26d96eda470c485
Ihtml <2483690020@qq.com>
2022-01-06 11:38:22 +0800

    feat(graph): dfstraverse

--- Code/05图/04邻接表深度和广度遍历.cpp
+++ Code/05图/04邻接表深度和广度遍历.cpp
@@ -180,3 +180,13 @@ void DFS(GraphAdjList GL, int i) {
         p = p->next;
     }
 }
+
+/* 邻接表的深度遍历操作 */
+void DFSTraverse(GraphAdjList GL) {
+    int i;
+    for (i = 0; i < GL->numVertexes; i++)
+        visited[i] = FALSE; /* 初始所有顶点状态都是未访问过状态 */
+    for (i = 0; i < GL->numVertexes; i++)
+        if (!visited[i]) /* 对未访问过的顶点调用DFS,若是连通图,只会执行一次 */
+            DFS(GL, i);
+}
