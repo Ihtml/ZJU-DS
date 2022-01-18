3cb2388f6f0b4858c670376a8c74f190b12a9839
Ihtml <2483690020@qq.com>
2022-01-18 10:32:06 +0800

    feat(graph): add test case

--- Code/05图/07最短路径Dijistra.cpp
+++ Code/05图/07最短路径Dijistra.cpp
@@ -105,3 +105,27 @@ void ShortestPath_Dijkstra(MGraph G, int v0, Patharc* P, ShortPathTable* D) {
         }
     }
 }
+
+int main(void) {
+    int i, j, v0;
+    MGraph G;
+    Patharc P;
+    ShortPathTable D; 
+    v0 = 0;
+
+    CreateMGraph(&G);
+    ShortestPath_Dijkstra(G, v0, &P, &D);
+
+    for (i = 1; i < G.numVertexes; ++i) {
+        printf("v%d - v%d : ", v0, i);
+        j = i;
+        while (P[j] != -1) {
+            printf("%d ", P[j]);
+            j = P[j];
+        }
+        printf("\n");
+    }
+    for (i = 1; i < G.numVertexes; ++i)
+        printf("v%d - v%d : %d \n", G.vexs[0], G.vexs[i], D[i]);
+    return 0;
+}
