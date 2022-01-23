a0b31f1e4c6ed6c0ee25216be1f926ae9fb7dc2a
Ihtml <2483690020@qq.com>
2022-01-23 14:21:16 +0800

    feat(graph): create graph

--- Code/05图/09拓扑排序.cpp
+++ Code/05图/09拓扑排序.cpp
@@ -41,3 +41,73 @@ typedef struct {
     int numVertexes, numEdges; /* 图中当前顶点数和边数 */
 } graphAdjList, *GraphAdjList;
 /* **************************** */
+
+void CreateMGraph(MGraph* G) /* 构件图 */
+{
+    int i, j;
+
+    /* printf("请输入边数和顶点数:"); */
+    G->numEdges = MAXEDGE;
+    G->numVertexes = MAXVEX;
+
+    for (i = 0; i < G->numVertexes; i++) /* 初始化图 */
+    {
+        G->vexs[i] = i;
+    }
+    for (i = 0; i < G->numVertexes; i++) /* 初始化图 */
+    {
+        for (j = 0; j < G->numVertexes; j++) {
+            G->arc[i][j] = 0;
+        }
+    }
+    G->arc[0][4] = 1;
+    G->arc[0][5] = 1;
+    G->arc[0][11] = 1;
+    G->arc[1][2] = 1;
+    G->arc[1][4] = 1;
+    G->arc[1][8] = 1;
+    G->arc[2][5] = 1;
+    G->arc[2][6] = 1;
+    G->arc[2][9] = 1;
+    G->arc[3][2] = 1;
+    G->arc[3][13] = 1;
+    G->arc[4][7] = 1;
+    G->arc[5][8] = 1;
+    G->arc[5][12] = 1;
+    G->arc[6][5] = 1;
+    G->arc[8][7] = 1;
+    G->arc[9][10] = 1;
+    G->arc[9][11] = 1;
+    G->arc[10][13] = 1;
+    G->arc[12][9] = 1;
+}
+
+/* 利用邻接矩阵构建邻接表 */
+void CreateALGraph(MGraph G, GraphAdjList* GL) {
+    int i, j;
+    EdgeNode* e;
+
+    *GL = (GraphAdjList)malloc(sizeof(graphAdjList));
+
+    (*GL)->numVertexes = G.numVertexes;
+    (*GL)->numEdges = G.numEdges;
+    for (i = 0; i < G.numVertexes; i++) /* 读入顶点信息，建立顶点表 */
+    {
+        (*GL)->adjList[i].in = 0;
+        (*GL)->adjList[i].data = G.vexs[i];
+        (*GL)->adjList[i].firstedge = NULL; /* 将边表置为空表 */
+    }
+
+    for (i = 0; i < G.numVertexes; i++) /* 建立边表 */
+    {
+        for (j = 0; j < G.numVertexes; j++) {
+            if (G.arc[i][j] == 1) {
+                e = (EdgeNode*)malloc(sizeof(EdgeNode));
+                e->adjvex = j; /* 邻接序号为j  */
+                (*GL)->adjList[i].firstedge; /* 将当前顶点上的指向的结点指针赋值给e */
+                (*GL)->adjList[i].firstedge = e; /* 将当前顶点的指针指向e  */
+                (*GL)->adjList[j].in++;
+            }
+        }
+    }
+}
