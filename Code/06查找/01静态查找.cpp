5491b9c043f5d90866a696bebe64193f4097c16a
Ihtml <2483690020@qq.com>
2022-02-07 12:39:45 +0800

    feat(search): fibonacci_Search

--- Code/06查找/01静态查找.cpp
+++ Code/06查找/01静态查找.cpp
@@ -66,3 +66,31 @@ int Interpolation_Search(int* a, int n, int key) {
     }
     return 0;
 }
+
+/* 斐波那契查找 */
+int Fibonacci_Search(int* a, int n, int key) {
+    int low, high, mid, i, k = 0;
+    low = 1;  /* 定义最低下标为记录首位 */
+    high = n; /* 定义最高下标为记录末位 */
+    while (n > F[k] - 1)
+        k++;
+    for (i = n; i < F[k] - 1; i++)
+        a[i] = a[n];
+
+    while (low <= high) {
+        mid = low + F[k - 1] - 1;
+        if (key < a[mid]) {
+            high = mid - 1;
+            k = k - 1;
+        } else if (key > a[mid]) {
+            low = mid + 1;
+            k = k - 2;
+        } else {
+            if (mid <= n)
+                return mid; /* 若相等则说明mid即为查找到的位置 */
+            else
+                return n;
+        }
+    }
+    return 0;
+}
