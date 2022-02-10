f34354d57e1fdd6fead37261e1923e6962b13ae2
Ihtml <2483690020@qq.com>
2022-02-10 09:58:38 +0800

    feat(search): insertBST

--- Code/06查找/02二叉排序树.cpp
+++ Code/06查找/02二叉排序树.cpp
@@ -37,3 +37,21 @@ Status SearchBST(BiTree T, int key, BiTree f, BiTree* p) {
     else
         return SearchBST(T->rchild, key, T, p); /*  在右子树中继续查找 */
 }
+
+Status InsertBST(BiTree *T, int key){
+    BiTree p, s;
+    if (!SearchBST(*T, key, NULL, &p)) /* 查找不成功 */
+    {
+        s = (BiTree)malloc(sizeof(BiTNode));
+        s->data = key;
+        s->lchild = s->rchild = NULL;
+        if (!p)
+            *T = s; /*  插入s为新的根结点 */
+        else if (key < p->data)
+            p->lchild = s; /*  插入s为左孩子 */
+        else
+            p->rchild = s; /*  插入s为右孩子 */
+        return TRUE;
+    } else
+        return FALSE; /*  树中已有关键字相同的结点，不再插入 */
+}
