0e2548234d737f0728041f49662d8abe6766ef95
Ihtml <2483690020@qq.com>
2022-02-13 09:41:43 +0800

    feat(bst): add test case

--- Code/06查找/02二叉排序树.cpp
+++ Code/06查找/02二叉排序树.cpp
@@ -103,3 +103,17 @@ Status DeleteBST(BiTree* T, int key) {
             return DeleteBST(&(*T)->rchild, key);
     }
 }
+
+int main(void) {
+    int i;
+    int a[10] = {62, 88, 58, 47, 35, 73, 51, 99, 37, 93};
+    BiTree T = NULL;
+
+    for (i = 0; i < 10; i++) {
+        InsertBST(&T, a[i]);
+    }
+    DeleteBST(&T, 93);
+    DeleteBST(&T, 47);
+    printf("本样例建议断点跟踪查看二叉排序树结构");
+    return 0;
+}
