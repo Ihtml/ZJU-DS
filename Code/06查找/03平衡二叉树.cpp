e970c26a5de1689ba8ff1f6071231b45ecea6c1b
Ihtml <2483690020@qq.com>
2022-02-16 09:57:23 +0800

    feat(search): aVL
    LeftBalance

--- Code/06查找/03平衡二叉树.cpp
+++ Code/06查找/03平衡二叉树.cpp
@@ -39,3 +39,38 @@ void L_Rotate(BiTree* P) {
     R->lchild = (*P);
     *P = R; /*  P指向新的根结点 */
 }
+
+#define LH +1 /*  左高 */
+#define EH 0  /*  等高 */
+#define RH -1 /*  右高 */
+
+/*  对以指针T所指结点为根的二叉树作左平衡旋转处理 */
+/*  本算法结束时，指针T指向新的根结点 */
+void LeftBalance(BiTree* T) {
+    BiTree L, Lr;
+    L = (*T)->lchild; /*  L指向T的左子树根结点 */
+    switch (L->bf) { /*  检查T的左子树的平衡度，并作相应平衡处理 */
+        case LH: /*  新结点插入在T的左孩子的左子树上，要作单右旋处理 */
+            (*T)->bf = L->bf = EH;
+            R_Rotate(T);
+            break;
+        case RH: /*  新结点插入在T的左孩子的右子树上，要作双旋处理 */
+            Lr = L->rchild;   /*  Lr指向T的左孩子的右子树根 */
+            switch (Lr->bf) { /*  修改T及其左孩子的平衡因子 */
+                case LH:
+                    (*T)->bf = RH;
+                    L->bf = EH;
+                    break;
+                case EH:
+                    (*T)->bf = L->bf = EH;
+                    break;
+                case RH:
+                    (*T)->bf = EH;
+                    L->bf = LH;
+                    break;
+            }
+            Lr->bf = EH;
+            L_Rotate(&(*T)->lchild); /*  对T的左子树作左旋平衡处理 */
+            R_Rotate(T);             /*  对T作右旋平衡处理 */
+    }
+}
