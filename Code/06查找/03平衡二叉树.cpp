86d42cd5fe4f1bd2c5c983f367e2b5a6a93bf943
Ihtml <2483690020@qq.com>
2022-02-17 09:59:48 +0800

    feat(avl): rightBalance

--- Code/06查找/03平衡二叉树.cpp
+++ Code/06查找/03平衡二叉树.cpp
@@ -74,3 +74,34 @@ void LeftBalance(BiTree* T) {
             R_Rotate(T);             /*  对T作右旋平衡处理 */
     }
 }
+
+/*  对以指针T所指结点为根的二叉树作右平衡旋转处理， */
+/*  本算法结束时，指针T指向新的根结点 */
+void RightBalance(BiTree* T) {
+    BiTree R, Rl;
+    R = (*T)->rchild; /*  R指向T的右子树根结点 */
+    switch (R->bf) { /*  检查T的右子树的平衡度，并作相应平衡处理 */
+        case RH: /*  新结点插入在T的右孩子的右子树上，要作单左旋处理 */
+            (*T)->bf = R->bf = EH;
+            L_Rotate(T);
+            break;
+        case LH: /*  新结点插入在T的右孩子的左子树上，要作双旋处理 */
+            Rl = R->lchild;   /*  Rl指向T的右孩子的左子树根 */
+            switch (Rl->bf) { /*  修改T及其右孩子的平衡因子 */
+                case RH:
+                    (*T)->bf = LH;
+                    R->bf = EH;
+                    break;
+                case EH:
+                    (*T)->bf = R->bf = EH;
+                    break;
+                case LH:
+                    (*T)->bf = EH;
+                    R->bf = RH;
+                    break;
+            }
+            Rl->bf = EH;
+            R_Rotate(&(*T)->rchild); /*  对T的右子树作右旋平衡处理 */
+            L_Rotate(T);             /*  对T作左旋平衡处理 */
+    }
+}
