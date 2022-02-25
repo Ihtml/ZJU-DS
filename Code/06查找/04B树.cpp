a8dd623d6bc052caba05a9c3c632e685963ae2be
Ihtml <2483690020@qq.com>
2022-02-25 10:12:06 +0800

    feat(btree): insert btree

--- Code/06查找/04B树.cpp
+++ Code/06查找/04B树.cpp
@@ -115,3 +115,30 @@ void NewRoot(BTree* T, int key, BTree ap) {
     if ((*T)->node[1].ptr)
         (*T)->node[1].ptr->parent = *T;
 }
+
+/*  在m阶B树T上结点*q的key[i]与key[i+1]之间插入关键字K的指针r。若引起 */
+/*  结点过大,则沿双亲链进行必要的结点分裂调整,使T仍是m阶B树。 */
+void InsertBTree(BTree* T, int key, BTree q, int i) {
+    BTree ap = NULL;
+    Status finished = FALSE;
+    int s;
+    int rx;
+    rx = key;
+    while (q && !finished) {
+        Insert(&q, i, rx, ap); /*  将r->key、r和ap分别插入到q->key[i+1]、q->recptr[i+1]和q->ptr[i+1]中
+                                */
+        if (q->keynum < m)
+            finished = TRUE; /*  插入完成 */
+        else {               /*  分裂结点*q */
+            s = (m + 1) / 2;
+            rx = q->node[s].recptr;
+            split(&q, &ap); /*  将q->key[s+1..m],q->ptr[s..m]和q->recptr[s+1..m]移入新结点*ap
+                             */
+            q = q->parent;
+            if (q)
+                i = Search(q, key); /*  在双亲结点*q中查找rx->key的插入位置  */
+        }
+    }
+    if (!finished) /*  T是空树(参数q初值为NULL)或根结点已分裂为结点*q和*ap */
+        NewRoot(T, rx, ap); /*  生成含信息(T,rx,ap)的新的根结点*T，原T和ap为子树指针 */
+}
