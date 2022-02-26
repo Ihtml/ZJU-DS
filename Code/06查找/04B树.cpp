4eaabdf6f37482e944cc55c6ac8ccac04461467f
Ihtml <2483690020@qq.com>
2022-02-26 12:57:07 +0800

    feat(btree): add main function

--- Code/06查找/04B树.cpp
+++ Code/06查找/04B树.cpp
@@ -142,3 +142,31 @@ void InsertBTree(BTree* T, int key, BTree q, int i) {
     if (!finished) /*  T是空树(参数q初值为NULL)或根结点已分裂为结点*q和*ap */
         NewRoot(T, rx, ap); /*  生成含信息(T,rx,ap)的新的根结点*T，原T和ap为子树指针 */
 }
+
+void print(BTNode c, int i) /*  TraverseDSTable()调用的函数  */
+{
+    printf("(%d)", c.node[i].key);
+}
+
+int main() {
+    int r[N] = {22, 16, 41, 58, 8,  11, 12, 16, 17,
+                22, 23, 31, 41, 52, 58, 59, 61};
+    BTree T = NULL;
+    Result s;
+    int i;
+    for (i = 0; i < N; i++) {
+        s = SearchBTree(T, r[i]);
+        if (!s.tag)
+            InsertBTree(&T, r[i], s.pt, s.i);
+    }
+    printf("\n请输入待查找记录的关键字: ");
+    scanf("%d", &i);
+    s = SearchBTree(T, i);
+    if (s.tag)
+        print(*(s.pt), s.i);
+    else
+        printf("没找到");
+    printf("\n");
+
+    return 0;
+}
