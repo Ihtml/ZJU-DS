13f515e2c370628eaca35d6128b069232282cda3
Ihtml <2483690020@qq.com>
2022-03-02 11:28:08 +0800

    feat(hashtable): add test case

--- Code/06查找/05散列表.cpp
+++ Code/06查找/05散列表.cpp
@@ -62,3 +62,29 @@ Status SearchHash(HashTable H, int key, int* addr) {
     }
     return SUCCESS;
 }
+
+int main() {
+    int arr[HASHSIZE] = {12, 67, 56, 16, 25, 37, 22, 29, 15, 47, 48, 34};
+    int i, p, key, result;
+    HashTable H;
+
+    key = 39;
+
+    InitHashTable(&H);
+    for (i = 0; i < m; i++)
+        InsertHash(&H, arr[i]);
+
+    result = SearchHash(H, key, &p);
+    if (result)
+        printf("查找 %d 的地址为：%d \n", key, p);
+    else
+        printf("查找 %d 失败。\n", key);
+
+    for (i = 0; i < m; i++) {
+        key = arr[i];
+        SearchHash(H, key, &p);
+        printf("查找 %d 的地址为：%d \n", key, p);
+    }
+
+    return 0;
+}
