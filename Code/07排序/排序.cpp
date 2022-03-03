ec9d696e5219ebeff276ef79b9e8dbf6605ba847
Ihtml <2483690020@qq.com>
2022-03-03 10:31:58 +0800

    feat(sort): define struct

---
+++ Code/07排序/排序.cpp
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_LENGTH_INSERT_SORT 7 /* 用于快速排序时判断是否选用插入排序阙值 */
+
+typedef int Status;
+
+#define MAXSIZE 10000 /* 用于要排序数组个数最大值，可根据需要修改 */
+typedef struct {
+    int r[MAXSIZE + 1]; /* 用于存储要排序数组，r[0]用作哨兵或临时变量 */
+    int length; /* 用于记录顺序表的长度 */
+} SqList;
+
+/* 交换L中数组r的下标为i和j的值 */
+void swap(SqList* L, int i, int j) {
+    int temp = L->r[i];
+    L->r[i] = L->r[j];
+    L->r[j] = temp;
+}
