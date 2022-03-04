b1504ee0236a428781108994c8c659ede8146233
Ihtml <2483690020@qq.com>
2022-03-04 10:48:54 +0800

    feat(sort): bubblesort0

--- Code/07排序/排序.cpp
+++ Code/07排序/排序.cpp
@@ -26,3 +26,23 @@ void swap(SqList* L, int i, int j) {
     L->r[i] = L->r[j];
     L->r[j] = temp;
 }
+
+void print(SqList L) {
+    int i;
+    for (i = 1; i < L.length; i++)
+        printf("%d,", L.r[i]);
+    printf("%d", L.r[i]);
+    printf("\n");
+}
+
+/* 对顺序表L作交换排序（冒泡排序初级版） */
+void BubbleSort0(SqList* L) {
+    int i, j;
+    for (i = 1; i < L->length; i++) {
+        for (j = i + 1; j <= L->length; j++) {
+            if (L->r[i] > L->r[j]) {
+                swap(L, i, j); /* 交换L->r[i]与L->r[j]的值 */
+            }
+        }
+    }
+}
