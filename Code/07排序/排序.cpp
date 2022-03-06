8b00cfe663e1dab417ef8b8de5a354c8b00eeb76
Ihtml <2483690020@qq.com>
2022-03-06 11:28:32 +0800

    feat(sort): bubbleSort2

--- Code/07排序/排序.cpp
+++ Code/07排序/排序.cpp
@@ -60,3 +60,19 @@ void BubbleSort(SqList* L) {
         }
     }
 }
+
+/* 对顺序表L作改进冒泡算法 */
+void BubbleSort2(SqList* L) {
+    int i, j;
+    Status flag = TRUE; /* flag用来作为标记 */
+    for (i = 1; i < L->length && flag; i++) /* 若flag为true说明有过数据交换，否则停止循环 */
+    {
+        flag = FALSE; /* 初始为False */
+        for (j = L->length - 1; j >= i; j--) { /* 循环结束后flag为false说明已从小到大排好序 */
+            if (L->r[j] > L->r[j + 1]) {
+                swap(L, j, j + 1); /* 交换L->r[j]与L->r[j+1]的值 */
+                flag = TRUE;       /* 如果有数据交换，则flag为true */
+            }
+        }
+    }
+}
