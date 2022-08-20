eb1ce03370b53c2fdb22e8b9e1582ea62d1c34be
Ihtml <2483690020@qq.com>
2022-08-20 15:08:33 +0800

    feat(algorithm): sqList add c++ code

---
+++ WangDao/2线性表/线性表.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+const int MaxSize = 50;
+// 线性表的顺序存储类型
+struct SqList {
+    int data[MaxSize];
+    int length;
+};
+// struct SqList list1; // C语言写法
+SqList list1;  // C++不用写struct
+
+int main() {
+    return 0;
+}
