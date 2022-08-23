b1b4d814590ddd23f68e2661076be7a30ce8fa0e
Ihtml <2483690020@qq.com>
2022-08-23 10:22:03 +0800

    feat(algorithm): listInsert

--- WangDao/2线性表/线性表.cpp
+++ WangDao/2线性表/线性表.cpp
@@ -34,6 +34,19 @@ struct SeqList {
 // SeqList SeqList1;
 // SeqList1.data = new int[InitSize];
 
+// 顺序表插入 
+bool ListInsert(SqList &L, int i, int e){
+    if(i<1 || i>L.length+1)  // 判断i的范围是否有效
+        return false;
+    if (L.length >= MaxSize) // 当前存储空间已满，不能插入
+        return false;
+    for (int j = L.length; j>=i; j--)  // 将第i个元素及之后的元素后移
+        L.data[j]=L.data[j-1];
+    L.data[i-1]=e;  // 在位置i处放入e
+    L.length++;  // 线性表长度加一
+    return true;
+}
+
 int main() {
     return 0;
 }
