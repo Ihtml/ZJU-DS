6608053bca4b23e05115092f8b8470a789437789
Ihtml <2483690020@qq.com>
2022-09-09 09:35:36 +0800

    feat(algorithm): update stack

--- WangDao/3栈队列和数组/栈.cpp
+++ WangDao/3栈队列和数组/栈.cpp
@@ -15,5 +15,17 @@ const int MaxSize = 50;
 // 栈的顺序存储类型
 typedef struct {
     int data[MaxSize];  // 存放栈中元素
-    int top;  // 栈顶指针
+    int top;  // 栈顶指针  初始时设置s.top=-1; 栈顶元素：s.data[s.top]
 } SqStack;
+
+// 初始化
+void InitStack(SqStack &S){
+    S.top=-1;
+}
+// 判栈空
+bool StackEmpty(SqStack S){
+    if (S.top == -1)  // 栈空
+        return true;
+    else   // 不空
+        return false;
+}
