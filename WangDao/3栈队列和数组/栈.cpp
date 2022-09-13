bc947218fd8614db5a7d4e89d9e820f41d1ea499
Ihtml <2483690020@qq.com>
2022-09-13 09:13:48 +0800

    feat(algorithm): link stack

--- WangDao/3栈队列和数组/栈.cpp
+++ WangDao/3栈队列和数组/栈.cpp
@@ -18,35 +18,40 @@ typedef struct {
     int top;  // 栈顶指针  初始时设置s.top=-1; 栈顶元素：s.data[s.top]
 } SqStack;
 
+typedef struct Linknode {
+    int data;               // 数据域
+    struct Linknode* next;  // 指针域
+} * LiStack;                // 栈类型定义
+
 // 初始化
-void InitStack(SqStack &S){
-    S.top=-1;
+void InitStack(SqStack& S) {
+    S.top = -1;
 }
 // 判栈空
-bool StackEmpty(SqStack S){
+bool StackEmpty(SqStack S) {
     if (S.top == -1)  // 栈空
         return true;
-    else   // 不空
+    else  // 不空
         return false;
 }
 // 进栈
-bool Push(SqStack &S, int x){
-    if (S.top == MaxSize-1)  // 栈满 报错
+bool Push(SqStack& S, int x) {
+    if (S.top == MaxSize - 1)  // 栈满 报错
         return false;
-    S.data[++S.top] = x;   // 指针先加1
+    S.data[++S.top] = x;  // 指针先加1
     return true;
 }
 // 出栈
-bool Pop(SqStack &S, int &x){
+bool Pop(SqStack& S, int& x) {
     if (S.top == -1)
         return false;
-    x=S.data[S.top--];  // 先出栈 指针再减一
+    x = S.data[S.top--];  // 先出栈 指针再减一
     return true;
 }
 
 // 读栈顶元素
-bool GetTop(SqStack S, int &x){
-    if (S.top==-1)
+bool GetTop(SqStack S, int& x) {
+    if (S.top == -1)
         return false;
-    x=S.data[S.top];
+    x = S.data[S.top];
 }
