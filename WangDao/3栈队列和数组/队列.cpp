c7f9a6b171441abea41c72702a756fcdc56876ae
Ihtml <2483690020@qq.com>
2022-09-15 09:41:51 +0800

    feat(algorithm): circular queue

--- WangDao/3栈队列和数组/队列.cpp
+++ WangDao/3栈队列和数组/队列.cpp
@@ -16,3 +16,15 @@ typedef struct {
     // 初始状态（队空条件） Q.front==Q.rear==0
     int front, rear;    // 队头指针和队尾指针
 } SqQueue;
+
+// 循环队列
+void InitQueue(SqQueue &Q){
+    Q.rear = Q.front = 0;  // 初始化队首、队尾指针
+}
+// 循环队列判队空
+bool isEmpty(SqQueue Q){
+    if (Q.rear==Q.front)
+        return true;
+    else
+        return false;
+}
