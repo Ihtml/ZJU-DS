658f69d29b7eaecb8808a839ac0ad7837dddbce0
Ihtml <2483690020@qq.com>
2022-09-24 09:53:38 +0800

    feat(string): update

--- WangDao/4串/string.cpp
+++ WangDao/4串/string.cpp
@@ -15,3 +15,9 @@ typedef struct {
     char ch[MaxSize];  // 每个分量存储一个字符
     int length;        //  串的实际长度
 } SString;
+
+// 堆分配存储表示
+typedef struct {
+    char* ch;    // 按串长分配存储区 ch指向串的基地址
+    int length;  // 串的长度
+} HString;
