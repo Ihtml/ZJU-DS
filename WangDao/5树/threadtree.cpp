c246c6113ddb5fd0cc328bae72a0be01e4c7a7fb
Ihtml <2483690020@qq.com>
2022-10-07 13:04:00 +0800

    feat(threadtree): inorder build threadtree

--- WangDao/5树/threadtree.cpp
+++ WangDao/5树/threadtree.cpp
@@ -16,3 +16,20 @@ typedef struct ThreadNode {
     struct ThreadNode *lchild, *rchild;  // 左、右孩子指针
     int ltag, rtag;
 } ThreadNode, *ThreadTree;
+
+// 通过中序遍历对二叉树线索化
+void InThread(ThreadTree& p, ThreadTree& pre) {
+    if (p != NULL) {
+        InThread(p->lchild, pre);  // 递归线索化左子树
+        if (p->lchild == NULL) {   // 左子树为空，建立前驱线索
+            p->lchild = pre;
+            p->ltag = 1;
+        }
+        if (pre != NULL && pre->rchild == NULL) {
+            pre->rchild = p;  // 建立前驱结点的后继线索
+            pre->rtag = 1;
+        }
+        pre = p;  // 标识当前结点为刚刚访问过的结点
+        InThread(p->rchild, pre);  // 递归 线索化右子树
+    }
+}
