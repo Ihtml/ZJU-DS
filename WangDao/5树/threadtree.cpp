70f60c615d204baf95b0e2d50c28303d33bc4c48
Ihtml <2483690020@qq.com>
2022-10-08 09:30:32 +0800

    feat(bitree): create inthread tree

--- WangDao/5树/threadtree.cpp
+++ WangDao/5树/threadtree.cpp
@@ -33,3 +33,12 @@ void InThread(ThreadTree& p, ThreadTree& pre) {
         InThread(p->rchild, pre);  // 递归 线索化右子树
     }
 }
+
+void CreateInThread(ThreadTree T) {
+    ThreadTree pre = NULL;
+    if (T != NULL) {         // 非空二叉树，线索化
+        InThread(T, pre);    // 线索化二叉树
+        pre->rchild = NULL;  // 处理遍历的最后一个结点
+        pre->rtag = 1;
+    }
+}
