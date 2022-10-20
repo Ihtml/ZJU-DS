840e2e9c52489a8e51ff13e5834ce2f59a7a8151
Ihtml <2483690020@qq.com>
2022-10-20 09:20:27 +0800

    feat(graph): aLGraph

--- WangDao/6图/graph.cpp
+++ WangDao/6图/graph.cpp
@@ -11,10 +11,24 @@
 #include <vector>
 using namespace std;
 const int MAXVertexNum = 100;  //  顶点数目最大值
-typedef char VertexType; //  顶点的数据类型
-typedef int EdgeType;  //  带权图中边上权值的数据类型
+typedef char VertexType;       //  顶点的数据类型
+typedef int EdgeType;          //  带权图中边上权值的数据类型
 typedef struct {
-    VertexType Vex[MAXVertexNum];   // 顶点表
-    EdgeType Edge[MAXVertexNum][MAXVertexNum];   // 邻接矩阵 边表
+    VertexType Vex[MAXVertexNum];               // 顶点表
+    EdgeType Edge[MAXVertexNum][MAXVertexNum];  // 邻接矩阵 边表
     int vexnum, arcnum;  // 图的当前顶点数和弧数
-}MGraph;
+} MGraph;
+
+typedef struct ArcNode {  // 边表结点
+    int adjvex;  // 该弧所指向的顶点位置
+    struct ArcNode* next;
+} ArcNode;
+
+typedef struct VNode {  // 顶点表结点
+    VertexType data;    // 顶点信息
+    ArcNode* first;     //  指向第一条依附该顶点的弧的指针
+} VNode, AdjList[MAXVertexNum];
+typedef struct {
+    AdjList vertices;    // 邻接表
+    int vexnum, arcnum;  // 图的顶点数和弧数
+} ALGraph;               // ALGraph是以邻接表存储的图类型
