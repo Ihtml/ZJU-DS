4af7f1b94bf186e354f310c577952f42a6617bfa
Ihtml <2483690020@qq.com>
2022-11-02 08:07:04 +0800

    feat(find): binary search

--- WangDao/7查找/find.cpp
+++ WangDao/7查找/find.cpp
@@ -16,9 +16,25 @@ typedef struct find {  // 查找表的数据结构
     int* elem;  // 元素存储空间基址，建表时按实际长度分配，0号单元留空
     int TableLen;
 } SSTable;
-int Search_Seq(SSTable ST, int key){
-    ST.elem[0]=key;   // 哨兵
+int Search_Seq(SSTable ST, int key) {
+    ST.elem[0] = key;  // 哨兵
     int i;
-    for(i=ST.TableLen; ST.elem[i]!=key; --i);
+    for (i = ST.TableLen; ST.elem[i] != key; --i)
+        ;
     return i;
+}
+
+int Binary_Search(SSTable L, int key) {
+    int low = 0, high = L.TableLen - 1, mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (L.elem[mid] == key) {
+            return mid;
+        } else if (L.elem[mid] > key) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
 }
