afea6524087368a3c7caa5cc446ba8152c4a1943
Ihtml <2483690020@qq.com>
2022-11-10 09:54:54 +0800

    feat(bst): search small

--- WangDao/7查找/find.cpp
+++ WangDao/7查找/find.cpp
@@ -129,3 +129,24 @@ void OutPut(BSTNode* bt, int k) {
         OutPut(bt->lchild, k);
     }
 }
+
+// 在以t为根的子树上寻找第k小的元素，返回其所在结点的指针
+// 在树结点中增加一个count数据成员，存储以该结点为根的子树的结点个数
+BSTNode* Search_Small(BSTNode* t, int k) {
+    if (k < 1 || k > t->count)
+        return NULL;
+    if (t->lchild == NULL) {
+        if (k == 1) {
+            return t;
+        } else {
+            return Search_Small(t->rchild, k - 1);
+        }
+    } else {
+        if (t->lchild->count == k - 1)
+            return t;
+        if (t->lchild->count > k - 1)
+            return Search_Small(t->lchild, k);
+        if (t->lchild->count < k - 1)
+            return Search_Small(t->rchild, k - (t->lchild->count + 1));
+    }
+}
