224e02c6ca1e04084e3db540702c907ed8a93a46
Ihtml <2483690020@qq.com>
2022-11-13 09:24:25 +0800

    feat(sort): bubblesort

--- WangDao/8排序/sort.cpp
+++ WangDao/8排序/sort.cpp
@@ -26,18 +26,35 @@ void InsertSort(int A[], int n) {
     }
 }
 
+// 最坏时间复杂度O(n^2)
 void ShellSort(int A[], int n) {
     // A[0]只是暂存单元，不是哨兵，当j<=0时，插入位置已到
     for (int dk = n / 2; dk >= 1; dk = dk / 2) {
         int i, j;
         for (i = dk + 1; i <= n; i++) {
             if (A[i] < A[i - dk]) {  // A[i]插入有序增量子表
-                A[0] = A[i];  // 暂存在A[0]
-                for (j = i - dk; j > 0 && A[0] < A[j]; j -= dk) {
-                    A[j + dk] = A[j]; // 记录后移,查找插入位置
+                A[0] = A[i];         // 暂存在A[0]
+                for (j = i - dk; j > 0 && A[0] < A[j]; j = j - dk) {
+                    A[j + dk] = A[j];  // 记录后移,查找插入位置
                 }
                 A[j + dk] = A[0];  // 插入
             }
+        }
+    }
+}
+
+// 冒泡排序
+void BubbleSort(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool flag = false;
+        for (int j = n - 1; j > i; j--) {
+            if (A[j - 1] > A[j]) {
+                swap(A[j - 1], A[j]);
+                flag = true;
+            }
+        }
+        if (flag == false) {  // 遍历后没有发生交换 说明表已经有序
+            return;
         }
     }
 }
