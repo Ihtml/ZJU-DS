033a121a72511751eaa6a5cf35af3a9ebd2e66bb
Ihtml <2483690020@qq.com>
2022-11-20 09:39:57 +0800

    feat(sort): headadjust

--- WangDao/8排序/sort.cpp
+++ WangDao/8排序/sort.cpp
@@ -130,3 +130,19 @@ void BuildMaxHeap(int A[], int len) {
         HeadAdjust(A, i, len);
     }
 }
+void HeadAdjust(int A[], int k, int len) {
+    // 将元素k为根的子树进行调整
+    A[0] = A[k];  // A[0]暂存子树的根结点
+    for (int i = 2 * k; i <= len; i = i * 2) {
+        if (i < len && A[i] < A[i + 1]) {
+            i++;  // 取key较大的子结点的下标
+        }
+        if (A[0] >= A[i]) {
+            break;  //筛选结束
+        } else {
+            A[k] = A[i];  // 将A[i]调整到双亲结点上
+            k = i;        // 修改k值，以便继续向下筛选
+        }
+    }
+    A[k] = A[0];  // 被筛选结点的值放入最终位置
+}
