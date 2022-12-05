a47ac440ee9bd5cdb0e9faac8cd4f65a33352ec1
Ihtml <2483690020@qq.com>
2022-12-05 08:54:24 +0800

    feat(sqlist): binarysearch+exchange+insert

--- WangDao/2线性表/线性表.cpp
+++ WangDao/2线性表/线性表.cpp
@@ -227,7 +227,7 @@ void Exchange(int A[], int m, int n, int arraySize) {
 
 void SearchExchangeInsert(int A[], int x) {
     int low = 0, high = n - 1, mid;
-    while (low <= high) {
+    while (low <= high) {  // 折半查找
         mid = (low + high) / 2;
         if (A[mid] == x)
             break;
@@ -235,6 +235,17 @@ void SearchExchangeInsert(int A[], int x) {
             low = mid + 1;
         else
             high = mid - 1;
+    }
+    if (A[mid] == x && mid != n - 1) { 
+        int t = A[mid];
+        A[mid] = A[mid + 1];
+        A[mid + 1] = t;
+    }
+    if (low > high) {  // 查找失败 插入数据元素
+        for (int i = n - 1; i > high; i--) {
+            A[i + 1] = A[i];
+        }
+        A[i + 1] = x;
     }
 }
 int main() {
