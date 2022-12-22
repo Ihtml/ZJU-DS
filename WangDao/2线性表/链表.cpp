465d3c5ab0d5c949453eed4654d0cf2404ebc604
Ihtml <2483690020@qq.com>
2022-12-22 10:13:00 +0800

    feat(linklist): search_1st_Common

--- WangDao/2线性表/链表.cpp
+++ WangDao/2线性表/链表.cpp
@@ -263,6 +263,34 @@ void RangeDelete(LinkList& L, int min, int max) {
         }
     }
 }
+
+// 找两个单链表的第一个公共结点
+LinkList Search_1st_Common(LinkList L1, int len1, LinkList L2, int len2) {
+    LinkList longList, shortList;
+    int dist;
+    if (len1 > len2) {
+        longList = L1->next;
+        shortList = L2->next;
+        dist = len1 - len2;
+    } else {
+        longList = L2->next;
+        shortList = L1->next;
+        dist = len2 - len1;
+    }
+    while (dist--) {
+        longList = longList->next;
+    }
+    while (longList != NULL) {
+        if (longList == shortList) {
+            return longList;
+        } else {
+            longList = longList->next;
+            shortList = shortList->next;
+        }
+    }
+    return NULL;
+}
+
 int main() {
     LNode* LN;
     List_HeadInsert(LN);
