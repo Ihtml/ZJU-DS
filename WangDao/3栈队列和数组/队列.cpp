5437a21834636eac1e026e55ac9137330c312a75
Ihtml <2483690020@qq.com>
2023-01-23 23:46:51 +0800

    feat(fib): recursive

--- WangDao/3栈队列和数组/队列.cpp
+++ WangDao/3栈队列和数组/队列.cpp
@@ -105,3 +105,13 @@ void Inverser(stack<int>& S, queue<int>& Q) {
         S.pop();
     }
 }
+
+int Fib(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return Fib(n - 1) + Fib(n - 2);
+    }
+}
